f9d8517cdbc2983474cef6a937fd464b32f375ca
Tim Schonberger <timothy.schonberger@gmail.com>
2020-03-23 20:14:31 -0400

    Fill in the rest of the .py recipe configs

---
+++ MainTest.py
@@ -0,0 +1,15 @@
+from RecipeTraverser import RecipeTraverser
+from RecipeConfig import RecipeConfig
+
+config = RecipeConfig().get_config_yaml()
+# config = RecipeConfig().get_ingredient_config()
+
+items = (
+    ('chest', 1),
+)
+
+recipe_traverser = RecipeTraverser()
+
+recipe_traverser.print_trees(items, config)
+print("Total Materials")
+print(recipe_traverser.get_ingredients_list(items, config))
